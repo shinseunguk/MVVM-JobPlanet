dc243df98f99df6b21875106fc400f1ba9bf1e14
신승욱 <ukseung.dev@incross.com>
2024-11-04 15:51:42 +0900

    Initial Commit

---
+++ JobPlanet-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobPlanet-MVVM
+//
+//  Created by ukseung.dev on 11/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
